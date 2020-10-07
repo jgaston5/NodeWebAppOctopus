8cb5b019e5d8e63b884d991b8df8801583ce14ca
Jared Gaston <Jared.Gaston@fcsamerica.com>
2020-10-07 16:37:21 -0500

    add an original

--- deploy-package.ps1
+++ deploy-package.ps1
@@ -1,1 +1,7 @@
 write-host starting install
+
+write-host  make the image
+docker build -t jgaston-octopus/node-web-app .
+
+write-host  run the image
+docker run -p 49160:3000 -d jgaston-octopus/node-web-app
